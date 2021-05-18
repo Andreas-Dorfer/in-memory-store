6a847988de41ed0503b1c918557d0363de52714a
Andreas Dorfer <41114919+Andreas-Dorfer@users.noreply.github.com>
2021-05-18 17:14:06 +0200

    Tests

---
+++ src/Restaurant/FTGO.Restaurant.Tests/Tests.fs
@@ -0,0 +1,11 @@
+namespace FTGO.Restaurant.Tests
+
+open System
+open Microsoft.VisualStudio.TestTools.UnitTesting
+
+[<TestClass>]
+type TestClass () =
+
+    [<TestMethod>]
+    member this.TestMethodPassing () =
+        Assert.IsTrue(true);
