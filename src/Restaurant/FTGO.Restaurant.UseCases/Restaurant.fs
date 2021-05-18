7c51e3fedef855bb64431234616f7a1f38b5e825
Andreas Dorfer <41114919+Andreas-Dorfer@users.noreply.github.com>
2021-05-18 13:22:17 +0200

    Restaurant service

---
+++ src/Restaurant/FTGO.Restaurant.UseCases/Restaurant.fs
@@ -0,0 +1,10 @@
+﻿namespace FTGO.Restaurant.UseCases
+
+open FTGO.Restaurant.BaseTypes
+
+type Restaurant = {
+    Id : RestaurantId
+    Name : string
+}
+
+type FindRestaurant = RestaurantId -> Restaurant option
---
+++ src/Restaurant/FTGO.Restaurant/Restaurant.fs
@@ -0,0 +1,8 @@
+﻿namespace FTGO.Restaurant
+
+open FTGO.Restaurant.UseCases
+
+module Restaurant =
+
+    let find : FindRestaurant =
+        fun _ -> None
