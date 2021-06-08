d8f6beeba73a2cd0bfc9e8575497f7c7ab48b44d
Andreas Dorfer <41114919+Andreas-Dorfer@users.noreply.github.com>
2021-06-08 17:34:37 +0200

    delete Common.Entities

--- src/Common/FTGO.Common.Entities/Versioned.fs
+++
@@ -1,7 +0,0 @@
-﻿namespace FTGO.Common.Entities
-
-open FTGO.Common.BaseTypes
-
-type CreateEntity<'entity, 'event> = 'entity * 'event list -> Async<Versioned<'entity>>
-
-type ReadEntity<'id, 'entity> = 'id -> Async<Versioned<'entity> option>
