1ed5b8dba0af4cd88303fd5d3be58b03a3c8febc
Andreas Dorfer <41114919+Andreas-Dorfer@users.noreply.github.com>
2021-06-12 14:12:06 +0200

    Cosmos test

--- src/Common/FTGO.Common.BaseTypes/ETag.fs
+++ src/Common/FTGO.Common.BaseTypes/ETag.fs
@@ -3,3 +3,4 @@
 type ETag = ETag of string
 
 type Versioned<'a> = Versioned of 'a * ETag
+    with member versioned.Value = let (Versioned (value, _)) = versioned in value
--- src/Restaurant/FTGO.Restaurant.Tests/CreateTests.fs
+++ src/Restaurant/FTGO.Restaurant.Tests/CreateTests.fs
@@ -1,34 +1,40 @@
 namespace FTGO.Restaurant.Tests
 
-open System.Collections.Generic
 open System.Threading.Tasks
 open Microsoft.VisualStudio.TestTools.UnitTesting
 open FTGO.Common.BaseTypes
-open FTGO.Restaurant.Events
 open FTGO.Restaurant.UseCases
 open FTGO.Restaurant
+open FTGO.Restaurant.CosmosDbEntities
+open Microsoft.Azure.Cosmos
 
 [<TestClass>]
 type CreateTests () =
 
+    [<Literal>]
+    let primaryKey = "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw=="
+
     [<TestMethod>]
     member _.Foo () =
         async {
-            let restaurants = List<_> ()
-            let events = Queue<RestaurantCreatedEvent> ()
-            let createEntity (restaurant, entityCreated) = async {
-                restaurant |> restaurants.Add
-                entityCreated |> events.Enqueue
-                return ETag "1"
-            }
-            let createRestaurant = RestaurantService.create createEntity
-            
-            let expectedName = NonEmptyString.create "My Restaurant" |> Option.get
-            let args : CreateRestaurantArgs = { Name = expectedName; Menu = [] }
+            use client = new CosmosClient("https://localhost:8081", primaryKey)
+            let db = client.GetDatabase("RestaurantService")
+            let container = db.GetContainer("Restaurant")
 
-            let! restaurant = args |> createRestaurant
+            let createEntity = RestaurantAggregateAdapter.create container
+            let readEntity = RestaurantAggregateAdapter.read container
+
+            let create = RestaurantService.create createEntity
+            let read = RestaurantService.read readEntity
+
             
-            Assert.AreEqual<_> (expectedName, restaurant.Name)
-            Assert.AreEqual<_> (1, events.Count)
+            let args = {
+                Name = "My Restaurant" |> NonEmptyString.create |> Option.get
+                Menu = []
+            }
+            let! createdRestaurant = args |> create
+            let! readRestaurant = createdRestaurant.Id.Value |> read
+
+            return ()
         }
         |> Async.StartAsTask :> Task
