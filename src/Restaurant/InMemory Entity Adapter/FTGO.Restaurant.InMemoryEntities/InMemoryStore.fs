8303045bc9e1748c26bf3331d78279bf283f0832
Andreas Dorfer <41114919+Andreas-Dorfer@users.noreply.github.com>
2021-07-01 17:58:32 +0200

    InMemory

---
+++ src/Restaurant/InMemory Entity Adapter/FTGO.Restaurant.InMemoryEntities/InMemoryStore.fs
@@ -0,0 +1,40 @@
+﻿namespace FTGO.Restaurant.InMemoryEntities
+
+open System.Collections.Concurrent
+
+type InMemoryStore<'id, 'entity> = private InMemoryStore of ConcurrentDictionary<'id, Entry<'entity>>
+
+module InMemoryStore =
+    
+    [<Literal>]
+    let private insertVersion = 1uL
+
+    let add (InMemoryStore store) (id, entity) =
+        let entry = entity |> Entry.create insertVersion
+        if store.TryAdd (id, entry) then
+            Some (entry.Value, entry.Version)
+        else
+            None
+
+    let get (InMemoryStore store) id =
+        match store.TryGetValue id with
+        | true, entry ->
+            if entry.Deleted then
+                None
+            else
+                Some (entry.Value, entry.Version)
+        | _ -> None
+
+    let getAll (InMemoryStore store) () =
+         store.Values
+         |> Seq.where (fun { Deleted = deleted } -> not deleted)
+         |> Seq.map (fun { Version = version; Value = entity } -> (entity, version))
+         |> List.ofSeq
+
+    //let update (InMemoryStore store) (id, entity, version) =
+    //    let inc version =
+    //        let next = version + 1uL
+    //        if next = errorVersion then
+    //            insertVersion
+    //        else
+    //            next
---
+++ src/Restaurant/InMemory Entity Adapter/FTGO.Restaurant.InMemoryEntities/RestaurantAggregateAdapter.fs
@@ -0,0 +1,8 @@
+﻿namespace FTGO.Restaurant.InMemoryEntities
+
+//open FTGO.Restaurant.Entities
+
+//module RestaurantAggregateAdapter =
+
+//    let create store : CreateRestaurantEntity =
+        
