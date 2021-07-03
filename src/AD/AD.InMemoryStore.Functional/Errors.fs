67b3f9af6feb7f7337e6782035abdbfc41c6b95e
Andreas Dorfer <41114919+Andreas-Dorfer@users.noreply.github.com>
2021-07-03 14:11:32 +0200

    refactoring

--- src/AD/AD.InMemoryStore.Functional/Errors.fs
+++ src/AD/AD.InMemoryStore.Functional/Errors.fs
@@ -1,10 +1,13 @@
 ﻿namespace AD.InMemoryStore.Functional
 
 type AddError<'key> = DuplicateKey of 'key
+
 type GetError<'key> = KeyNotFound of 'key
+
 type UpdateError<'key> =
     | KeyNotFound of 'key
     | VersionMismatch of 'key
+
 type RemoveError<'key> =
     | KeyNotFound of 'key
     | VersionMismatch of 'key
