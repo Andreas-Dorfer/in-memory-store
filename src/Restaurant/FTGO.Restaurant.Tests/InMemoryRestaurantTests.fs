30648829967af0dd51fbbb130aa5cb7814bfcdf0
Andreas Dorfer <41114919+Andreas-Dorfer@users.noreply.github.com>
2021-07-03 13:50:11 +0200

    InMemoryRestaurantTests

---
+++ src/Restaurant/FTGO.Restaurant.Tests/InMemoryRestaurantTests.fs
@@ -0,0 +1,24 @@
+﻿namespace FTGO.Restaurant.Tests
+
+open Microsoft.VisualStudio.TestTools.UnitTesting
+open FTGO.Restaurant.BaseTypes
+open FTGO.Restaurant.Entities
+open AD.InMemoryStore.Functional
+open FTGO.Restaurant.InMemoryEntities
+
+[<TestClass>]
+type InMemoryRestaurantTests () =
+    inherit RestaurantTests<InMemoryStore<RestaurantId, RestaurantEntity>> ()
+
+    static do
+        RestaurantTests<_>.Init ()
+
+    override _.Context () = new TestDependency<_> (InMemoryStore<_, _> ())
+
+    override _.CreateEntityDependency store =
+        let createEntity = RestaurantAggregateAdapter.create store
+        new TestDependency<_> (createEntity)
+
+    override _.ReadEntityDependency store =
+        let readEntity = RestaurantAggregateAdapter.read store
+        new TestDependency<_> (readEntity)
--- src/Restaurant/InMemory Entity Adapter/FTGO.Restaurant.InMemoryEntities/RestaurantAggregateAdapter.fs
+++ src/Restaurant/InMemory Entity Adapter/FTGO.Restaurant.InMemoryEntities/RestaurantAggregateAdapter.fs
@@ -1,8 +1,24 @@
 ﻿namespace FTGO.Restaurant.InMemoryEntities
 
-//open FTGO.Restaurant.Entities
+open FTGO.Common.BaseTypes
+open FTGO.Restaurant.Entities
+open AD.InMemoryStore
+open AD.InMemoryStore.Functional
 
-//module RestaurantAggregateAdapter =
+module RestaurantAggregateAdapter =
 
-//    let create store : CreateRestaurantEntity =
-        
+    let private toEtag (version : Version) = version.ToString() |> ETag
+
+    let create (store : InMemoryStore<_, _>) : CreateRestaurantEntity =
+        fun (restaurant, _) -> async {
+            match (restaurant.Id, restaurant) |> store.Add with
+            | Ok (restaurant, version) -> return Versioned (restaurant, version |> toEtag)
+            | Error (AddError.DuplicateKey key) -> return invalidArg (nameof restaurant) (key.ToString())
+        }
+
+    let read (store : InMemoryStore<_, _>) : ReadRestaurantEntity =
+        fun id -> async {
+            match id |> store.Get with
+            | Ok (restaurant, version) -> return Some (Versioned (restaurant, version |> toEtag))
+            | Error (GetError.KeyNotFound _) -> return None
+        }
