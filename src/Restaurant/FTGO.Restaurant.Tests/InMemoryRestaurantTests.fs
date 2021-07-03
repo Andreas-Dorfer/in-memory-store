aaaeae6df5ae0325ba6f878a7ee289d057fc9e0a
Andreas Dorfer <41114919+Andreas-Dorfer@users.noreply.github.com>
2021-07-03 18:20:46 +0200

    Stores

--- src/Restaurant/FTGO.Restaurant.Tests/InMemoryRestaurantTests.fs
+++ src/Restaurant/FTGO.Restaurant.Tests/InMemoryRestaurantTests.fs
@@ -1,24 +1,21 @@
 ﻿namespace FTGO.Restaurant.Tests
 
 open Microsoft.VisualStudio.TestTools.UnitTesting
-open FTGO.Restaurant.BaseTypes
-open FTGO.Restaurant.Entities
-open AD.InMemoryStore.Functional
 open FTGO.Restaurant.InMemoryEntities
 
 [<TestClass>]
 type InMemoryRestaurantTests () =
-    inherit RestaurantTests<InMemoryStore<RestaurantId, RestaurantEntity>> ()
+    inherit RestaurantTests<Stores> ()
 
     static do
         RestaurantTests<_>.Init ()
 
-    override _.Context () = new TestDependency<_> (InMemoryStore<_, _> ())
+    override _.Context () = new TestDependency<_> (Stores.create ())
 
-    override _.CreateEntityDependency store =
-        let createEntity = RestaurantAggregateAdapter.create store
+    override _.CreateEntityDependency stores =
+        let createEntity = RestaurantAggregateAdapter.create stores
         new TestDependency<_> (createEntity)
 
-    override _.ReadEntityDependency store =
-        let readEntity = RestaurantAggregateAdapter.read store
+    override _.ReadEntityDependency stores =
+        let readEntity = RestaurantAggregateAdapter.read stores
         new TestDependency<_> (readEntity)
--- src/Restaurant/InMemory Entity Adapter/FTGO.Restaurant.InMemoryEntities/RestaurantAggregateAdapter.fs
+++ src/Restaurant/InMemory Entity Adapter/FTGO.Restaurant.InMemoryEntities/RestaurantAggregateAdapter.fs
@@ -1,24 +1,41 @@
 ﻿namespace FTGO.Restaurant.InMemoryEntities
 
+open System
 open FTGO.Common.BaseTypes
+open FTGO.Restaurant.BaseTypes
+open FTGO.Restaurant.Events
 open FTGO.Restaurant.Entities
 open AD.InMemoryStore
 open AD.InMemoryStore.Functional
+
+type Stores = {
+    Restaurants : InMemoryStore<RestaurantId, RestaurantEntity>
+    Events : InMemoryStore<Guid, RestaurantCreatedEvent>
+}
+
+module Stores =
+    let create () = {
+        Restaurants = InMemoryStore<RestaurantId, RestaurantEntity> ()
+        Events = InMemoryStore<Guid, RestaurantCreatedEvent> ()
+    }
 
 module RestaurantAggregateAdapter =
 
     let private toEtag (version : Version) = version.ToString() |> ETag
 
-    let create (store : InMemoryStore<_, _>) : CreateRestaurantEntity =
-        fun (restaurant, _) -> async {
-            match (restaurant.Id, restaurant) |> store.Add with
-            | Ok (restaurant, version) -> return Versioned (restaurant, version |> toEtag)
+    let create stores : CreateRestaurantEntity =
+        fun (restaurant, created) -> async {
+            match (restaurant.Id, restaurant) |> stores.Restaurants.Add with
+            | Ok (restaurant, version) ->
+                match (created.Id, created) |> stores.Events.Add with
+                | Ok _ -> return Versioned (restaurant, version |> toEtag)
+                | Error (AddError.DuplicateKey key) -> return invalidArg (nameof created) (key.ToString())
             | Error (AddError.DuplicateKey key) -> return invalidArg (nameof restaurant) (key.ToString())
         }
 
-    let read (store : InMemoryStore<_, _>) : ReadRestaurantEntity =
+    let read { Restaurants = restaurants } : ReadRestaurantEntity =
         fun id -> async {
-            match id |> store.Get with
+            match id |> restaurants.Get with
             | Ok (restaurant, version) -> return Some (Versioned (restaurant, version |> toEtag))
             | Error (GetError.KeyNotFound _) -> return None
         }
