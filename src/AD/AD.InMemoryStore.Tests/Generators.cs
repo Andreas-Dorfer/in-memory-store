d7a15be82a75617e4617a1f98798031f2ba11693
Andreas Dorfer <41114919+Andreas-Dorfer@users.noreply.github.com>
2021-07-07 20:33:01 +0200

    refactoring

--- src/AD/AD.InMemoryStore.Tests/Generators.cs
+++ src/AD/AD.InMemoryStore.Tests/Generators.cs
@@ -9,13 +9,11 @@ namespace AD.InMemoryStore.Tests
     {
         class ValueTupleArbitrary<T1, T2> : Arbitrary<(T1, T2)>
         {
-            static (T1, T2) ToValueTuple(Tuple<T1, T2> tuple) => (tuple.Item1, tuple.Item2);
-
             public override Gen<(T1, T2)> Generator =>
-                Arb.Generate<Tuple<T1, T2>>().Select(ToValueTuple);
+                Arb.Generate<Tuple<T1, T2>>().Select(_ => _.ToValueTuple());
 
-            public override IEnumerable<(T1, T2)> Shrinker((T1, T2) _arg1) =>
-                Arb.Shrink(Tuple.Create(_arg1.Item1, _arg1.Item2)).Select(ToValueTuple);
+            public override IEnumerable<(T1, T2)> Shrinker((T1, T2) arg) =>
+                Arb.Shrink(arg.ToTuple()).Select(_ => _.ToValueTuple());
         }
 
         public static Arbitrary<(T1, T2)> ValueTuple2<T1, T2>() => new ValueTupleArbitrary<T1, T2>();
