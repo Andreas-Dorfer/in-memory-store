0b95ad17b109248af49490f773acd78b35c51b90
Andreas Dorfer <41114919+Andreas-Dorfer@users.noreply.github.com>
2021-10-20 11:28:02 +0200

    functional Version

---
+++ src/AD.InMemoryStore.Functional.Tests/VersionTests.fs
@@ -0,0 +1,17 @@
+﻿namespace AD.InMemoryStore.Functional.Tests
+
+open Microsoft.VisualStudio.TestTools.UnitTesting
+open AD.InMemoryStore.Functional
+
+[<TestClass>]
+type VersionTests () =
+    
+    let store = KeyValueStore ()
+
+    [<TestMethod>]
+    member _.``To string and parse round trip`` () =
+        match (1, "A") |> store.Add with
+        | Error _ -> Assert.Fail "Ok expected"
+        | Ok (_, _, expected) ->
+            let actual = expected.ToString() |> Version.parse
+            Assert.AreEqual<_> (Some expected, actual)
--- src/AD.InMemoryStore.Functional/Version.fs
+++ src/AD.InMemoryStore.Functional/Version.fs
@@ -4,3 +4,16 @@
 [<Struct>]
 type Version = internal Version of AD.InMemoryStore.Version
     with override this.ToString () = let (Version version) = this in version.ToString()
+
+/// A value's version.
+module Version =
+
+    /// <summary>
+    /// Converts a textual representation to a version.
+    /// </summary>
+    /// <param name="text">The textual representation.</param>
+    /// <returns>The version.</returns>
+    let parse text =
+        match text |> AD.InMemoryStore.Version.TryParse with
+        | (true, version) -> Some (Version version)
+        | (false, _) -> None
